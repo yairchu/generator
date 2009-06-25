f0afa5e561136d50acf2b247c240937c11fc9db8
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-06-26 01:38:58 +0300

    added ifoldl, ifilter, and iTakeWhile

--- Iterator/Tools.hs
+++ Iterator/Tools.hs
@@ -1,16 +1,26 @@
 {-# OPTIONS -O2 -Wall #-}
 
 module Iterator.Tools (
-  ifoldr, imap, itake
+  ifoldr, ifoldl, imap, ifilter, itake, iTakeWhile
   ) where
 
 import Iterator
 
 import Control.Monad.Trans (lift)
 
+ifoldl :: (Monad m) => (a -> b -> a) -> a -> Iterator b m -> m a
+ifoldl func startVal =
+  evalIteratesT (r =<< next)
+  where
+    r Nothing = return startVal
+    r (Just v) = do
+      rest <- takeRest
+      lift $ ifoldl func (func startVal v) rest
+
+-- consFunc takes "m b" and not "b" so could avoid running the rest
 ifoldr :: (Monad m) => (a -> m b -> m b) -> m b -> Iterator a m -> m b
-ifoldr consFunc nilFunc iter =
-  evalIteratesT (r =<< next) iter
+ifoldr consFunc nilFunc =
+  evalIteratesT (r =<< next)
   where
     r Nothing = lift nilFunc
     r (Just v) = do
@@ -19,6 +29,18 @@ ifoldr consFunc nilFunc iter =
 
 imap :: Monad m => (a -> b) -> Iterator a m -> Iterator b m
 imap func = iterator . ifoldr (cons . func) nil
+
+ifilter :: Monad m => (a -> Bool) -> Iterator a m -> Iterator a m
+ifilter func =
+  iterator . ifoldr r nil
+  where
+    r x xs = if func x then cons x xs else xs
+
+iTakeWhile :: Monad m => (a -> Bool) -> Iterator a m -> Iterator a m
+iTakeWhile func =
+  iterator . ifoldr r nil
+  where
+    r x xs = if func x then cons x xs else nil
 
 itake :: (Monad m, Integral i) => i -> Iterator a m -> Iterator a m
 itake 0 _ = iterator nil
