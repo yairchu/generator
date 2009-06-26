ee1c1d89e6e66a2494a67b34d6fd05cb34841418
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-06-26 13:17:23 +0300

    made ifoldl strict and added toList

--- src/Data/Iterator/Tools.hs
+++ src/Data/Iterator/Tools.hs
@@ -1,13 +1,19 @@
 {-# OPTIONS -O2 -Wall #-}
 
 module Data.Iterator.Tools (
-  ifoldr, ifoldl, imap, ifilter, itake, iTakeWhile
+  ifoldr, ifoldl, imap, ifilter, itake, iTakeWhile, toList
   ) where
 
-import Data.Iterator
-
+import Control.Monad (liftM)
 import Control.Monad.Trans (lift)
+import Data.Iterator (
+  Iterator, cons, cons', evalIteratesT,
+  iterator, next, nil, takeRest)
 
+-- naming: for everything that's in prelude I add an "i" prefix,
+-- for convinient importing
+
+-- a strict foldl
 ifoldl :: (Monad m) => (a -> b -> a) -> a -> Iterator b m -> m a
 ifoldl func startVal =
   evalIteratesT (r =<< next)
@@ -15,7 +21,8 @@ ifoldl func startVal =
     r Nothing = return startVal
     r (Just v) = do
       rest <- takeRest
-      lift $ ifoldl func (func startVal v) rest
+      let x = func startVal v
+      seq x . lift $ ifoldl func x rest
 
 -- consFunc takes "m b" and not "b" so could avoid running the rest
 ifoldr :: (Monad m) => (a -> m b -> m b) -> m b -> Iterator a m -> m b
@@ -35,6 +42,10 @@ ifilter func =
   iterator . ifoldr r nil
   where
     r x xs = if func x then cons x xs else xs
+
+-- uses ifoldl because I think with ifoldr it would use much mem, right?
+toList :: (Monad m) => Iterator a m -> m [a]
+toList = liftM reverse . ifoldl (flip (:)) []
 
 iTakeWhile :: Monad m => (a -> Bool) -> Iterator a m -> Iterator a m
 iTakeWhile func =
