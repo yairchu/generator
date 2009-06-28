88ec82930d885a1575048cd6ef71441e3844f4a5
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-06-28 17:10:29 +0300

    fixed ProducerT's bind order problem

--- src/Control/Generator/ProducerT.hs
+++ src/Control/Generator/ProducerT.hs
@@ -4,35 +4,31 @@ module Control.Generator.ProducerT(
   ProducerT, produce, yield
   ) where
 
-import Control.Generator(Producer, cons, empty, mmerge)
-import Control.Generator.Tools(append)
-import Control.Monad(join, liftM)
-import Control.Monad.Trans(MonadTrans, MonadIO(..), lift)
+import Control.Generator (Producer, cons, empty, mmerge)
+import Control.Monad.Cont (Cont (..))
+import Control.Monad.Trans (MonadTrans(..), MonadIO(..))
 
-newtype ProducerT v m a = ProducerT (m (Producer v m, m a))
+newtype ProducerT v m a =
+  ProducerT { unProducerT :: Cont (Producer v m) a }
 
 instance Monad m => Monad (ProducerT v m) where
-  return = ProducerT . return . ((,) empty) . return
-  (ProducerT a) >>= f =
-    ProducerT $ do
-    (prodA, monA) <- a
-    valA <- monA
-    let ProducerT r = f valA
-        prod = append prodA . mmerge . liftM fst $ r
-        mon = join . liftM snd $ r
-    return (prod, mon)
+  return = ProducerT . return
+  (ProducerT a) >>= f = ProducerT $ a >>= unProducerT . f
   fail = ProducerT . fail
 
 instance MonadTrans (ProducerT v) where
-  lift = ProducerT . return . ((,) empty)
+  lift m =
+    ProducerT . Cont $
+      \f -> mmerge $ return . f =<< m
 
 instance MonadIO m => MonadIO (ProducerT v m) where
   liftIO = lift . liftIO
 
 yield :: Monad m => v -> ProducerT v m ()
-yield v = ProducerT $ return (cons v empty, return ())
+yield v =
+  ProducerT . Cont $
+    \f -> cons v $ f ()
 
 produce :: Monad m => ProducerT v m () -> Producer v m
-produce (ProducerT prodT) =
-  mmerge $ return . fst =<< prodT
+produce (ProducerT (Cont prod)) = prod $ const empty
 
