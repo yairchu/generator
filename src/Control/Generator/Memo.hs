a78fa5a2f2e1c46e1cc6e0818f5f9fa37ecabe68
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-06 02:07:17 +0300

    Revert "ugh. memo is general now"
    This reverts commit ac91c626c18953685f9887ff8a10b5bcb457ff89.

--- src/Control/Generator/Memo.hs
+++ src/Control/Generator/Memo.hs
@@ -1,5 +1,4 @@
 {-# OPTIONS -O2 -Wall #-}
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, Rank2Types, UndecidableInstances #-}
 
 module Control.Generator.Memo (memo) where
 
@@ -9,27 +8,21 @@ import Control.Monad (liftM)
 import Control.Monad.Trans (MonadIO(..))
 import Data.MRef (
   newDefaultMRef, putDefaultMRef, takeDefaultMRef)
-import Data.MRef.Classes (DefaultMRef, NewMRef, PutMRef, TakeMRef)
 
-class (DefaultMRef r m a, NewMRef r m a, PutMRef r m a, TakeMRef r m a) => MRef r m a
-instance (DefaultMRef r m a, NewMRef r m a, PutMRef r m a, TakeMRef r m a) => MRef r m a
-
-memoM :: (Monad m, Monad s, MRef r m (Maybe a)) =>
-  (forall x. m x -> s x) -> s a -> m (s a)
-memoM liftFunc action = do
+memoIO :: MonadIO m => m a -> IO (m a)
+memoIO action = do
   ref <- newDefaultMRef Nothing
   return $ do
-    x <- liftFunc $ takeDefaultMRef ref
+    x <- liftIO $ takeDefaultMRef ref
     case x of
       Just res -> return res
       Nothing -> do
         res <- action
-        liftFunc . putDefaultMRef ref $ Just res
+        liftIO . putDefaultMRef ref $ Just res
         return res
 
-memo :: (Monad m, Monad s, MRef r m (Maybe a)) =>
-  (forall x. m x -> s x) -> Producer m a -> IO (Producer m b)
-memo liftFunc =
-  liftM mmerge . memoM liftFunc .
-  transformProdMonad (liftIO . memoM liftFunc)
+memo :: MonadIO m => Producer m v -> IO (Producer m v)
+memo =
+  liftM mmerge . memoIO .
+  transformProdMonad (liftIO . memoIO)
 
