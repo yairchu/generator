03174a97728e9cddda7800281a4e2b5b72030417
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-06 02:07:54 +0300

    Revert "towards memo not just for IO"
    This reverts commit 04e46deb5d3512056a5b17cabd997e140e6c830f.

--- src/Control/Generator/Memo.hs
+++ src/Control/Generator/Memo.hs
@@ -2,23 +2,22 @@
 
 module Control.Generator.Memo (memo) where
 
+import Control.Concurrent.MVar (newMVar, putMVar, takeMVar)
 import Control.Generator (Producer, mmerge)
 import Control.Generator.Tools (transformProdMonad)
 import Control.Monad (liftM)
 import Control.Monad.Trans (MonadIO(..))
-import Data.MRef (
-  newDefaultMRef, putDefaultMRef, takeDefaultMRef)
 
 memoIO :: MonadIO m => m a -> IO (m a)
 memoIO action = do
-  ref <- newDefaultMRef Nothing
+  ref <- liftIO $ newMVar Nothing
   return $ do
-    x <- liftIO $ takeDefaultMRef ref
+    x <- liftIO $ takeMVar ref
     case x of
       Just res -> return res
       Nothing -> do
         res <- action
-        liftIO . putDefaultMRef ref $ Just res
+        liftIO . putMVar ref $ Just res
         return res
 
 memo :: MonadIO m => Producer m v -> IO (Producer m v)
