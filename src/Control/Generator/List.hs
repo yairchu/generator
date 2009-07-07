8790ae449ea204724940a60bdc0b896de84ec0a3
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-07 12:35:09 +0300

    added bestFirstSearch

--- src/Control/Generator/List.hs
+++ src/Control/Generator/List.hs
@@ -1,4 +1,6 @@
-module Control.Generator.List (bfs, bfsLayers, dfs) where
+module Control.Generator.List (
+  dfs, bfs, bfsLayers, bestFirstSearch
+  ) where
 
 import Control.Generator (
   Producer, evalConsumerT, next, processRest)
@@ -24,11 +26,30 @@ bfsLayers prod =
     case mx of
       Nothing -> lift []
       Just x ->
-        liftM (([x] :) . process) $
-        processRest rest
+        liftM (([x] :) . process) $ processRest rest
   where
     process = map concat . transpose
 
 bfs :: Producer [] a -> [a]
 bfs = concat . bfsLayers
+
+mergeOn :: Ord b => (a -> b) -> [[a]] -> [a]
+mergeOn f =
+  foldl merge2 []
+  where
+    merge2 (x:xs) (y:ys)
+      | f y > f x = x : merge2 xs (y:ys)
+      | otherwise = y : merge2 (x:xs) ys
+    merge2 xs ys = xs ++ ys
+
+bestFirstSearch :: Ord n => Producer [] (a, n) -> [a]
+bestFirstSearch prod =
+  map fst . process . (`evalConsumerT` prod) . fix $ \rest -> do
+    mx <- next
+    case mx of
+      Nothing -> lift []
+      Just x -> do
+        liftM ((x :) . process) $ processRest rest
+  where
+    process = mergeOn snd
 
