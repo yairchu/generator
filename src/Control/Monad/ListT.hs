2029973695baa8d5f8774b877901c7b8eacd11d6
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-10 10:05:08 +0300

    Created List typeclass. Should move to Data.List.Class

--- src/Control/Monad/ListT.hs
+++ src/Control/Monad/ListT.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE FlexibleInstances, FunctionalDependencies, MultiParamTypeClasses #-}
+
 module Control.Monad.ListT (
-  ListItem(..), ListT(..), cons, fromList
+  List(..), ListItem(..), ListT(..),
+  cons, fromList, filter, takeWhile,
+  foldrL, foldlL
 ) where
 
 import Control.Applicative (Applicative(..))
 import Control.Monad (MonadPlus(..), ap)
+import Control.Monad.Identity (Identity(..))
 import Control.Monad.Trans (MonadTrans(..))
 import Data.Foldable (Foldable, foldl')
 import Data.Monoid (Monoid(..))
+
+import Prelude hiding (filter, takeWhile)
 
 data ListItem l a =
   Nil |
@@ -15,22 +22,19 @@ data ListItem l a =
 data ListT m a = ListT { runListT :: m (ListItem (ListT m) a) }
 
 -- for mappend, fmap, bind
-foldrL :: Monad m =>
+foldrL' :: Monad m =>
   (a -> ListT m b -> ListT m b) -> ListT m b -> ListT m a -> ListT m b
-foldrL consFunc nilFunc list =
-  ListT $ do
-    item <- runListT list
-    runListT $ case item of
-      Nil -> nilFunc
-      Cons x xs -> 
-        consFunc x $ foldrL consFunc nilFunc xs
+foldrL' consFunc nilFunc =
+  ListT . foldrL step (runListT nilFunc)
+  where
+    step x = runListT . consFunc x . ListT
 
 instance Monad m => Monoid (ListT m a) where
   mempty = ListT $ return Nil
-  mappend = flip (foldrL cons)
+  mappend = flip (foldrL' cons)
 
 instance Monad m => Functor (ListT m) where
-  fmap func = foldrL (cons . func) mempty
+  fmap func = foldrL' (cons . func) mempty
 
 instance Monad m => Applicative (ListT m) where
   pure = return
@@ -38,7 +42,7 @@ instance Monad m => Applicative (ListT m) where
 
 instance Monad m => Monad (ListT m) where
   return = ListT . return . (`Cons` mempty)
-  a >>= b = foldrL mappend mempty $ fmap b a
+  a >>= b = foldrL' mappend mempty $ fmap b a
 
 instance Monad m => MonadPlus (ListT m) where
   mzero = mempty
@@ -52,4 +56,52 @@ cons = mplus . return
 
 fromList :: (MonadPlus m, Foldable t) => t a -> m a
 fromList = foldl' (flip (mplus . return)) mzero
+
+class (MonadPlus m, Monad i) => List m i | m -> i where
+  joinL :: i (m b) -> m b
+  unCons :: m a -> i (ListItem m a)
+
+instance Monad m => List (ListT m) m where
+  joinL = ListT . (>>= runListT)
+  unCons = runListT
+
+instance List [] Identity where
+  joinL = runIdentity
+  unCons [] = Identity Nil
+  unCons (x : xs) = Identity $ Cons x xs
+
+-- for foldrL and foldlL
+fold' :: List m i => (a -> m a -> i b) -> i b -> m a -> i b
+fold' step nilFunc list = do
+  item <- unCons list
+  case item of
+    Nil -> nilFunc
+    Cons x xs -> step x xs
+
+foldrL :: List m i => (a -> i b -> i b) -> i b -> m a -> i b
+foldrL consFunc nilFunc = 
+  fold' step nilFunc
+  where
+    step x = consFunc x . foldrL consFunc nilFunc
+
+foldlL :: List m i => (a -> b -> a) -> a -> m b -> i a
+foldlL step startVal =
+  fold' step' $ return startVal
+  where
+    step' x = foldlL step $ step startVal x
+
+-- for filter and takeWhile
+filter' :: List m i => (i (m a) -> i (m a)) -> (a -> Bool) -> m a -> m a
+filter' falseFunc cond =
+  joinL . foldrL step (return mzero)
+  where
+    step x xs
+      | cond x = return . cons x $ joinL xs
+      | otherwise = falseFunc xs
+
+filter :: List m i => (a -> Bool) -> m a -> m a
+filter = filter' id
+
+takeWhile :: List m i => (a -> Bool) -> m a -> m a
+takeWhile = filter' . const $ return mzero
 
