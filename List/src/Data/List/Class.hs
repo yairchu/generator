2a9d68359fdfb3de107a57edf26e7fc16da27cf1
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-09-22 00:41:42 +0300

    simplified List type-class

--- List/src/Data/List/Class.hs
+++ List/src/Data/List/Class.hs
@@ -11,7 +11,7 @@ module Data.List.Class (
   takeWhile, genericTake, scanl,
   transpose, zip, zipWith,
   -- | Non standard List operations
-  foldlL, toList, lengthL, lastL,
+  foldrL, foldlL, toList, lengthL, lastL,
   merge2On, mergeOn,
   -- | Operations useful for monadic lists
   execute, joinM,
@@ -19,11 +19,12 @@ module Data.List.Class (
   convList, transformListMonad, liftListMonad
   ) where
 
-import Control.Monad (MonadPlus(..), ap, join, liftM)
+import Control.Monad (MonadPlus(..), liftM)
 import Control.Monad.Identity (Identity(..))
-import Control.Monad.ListT (ListT(..), ListItem(..), foldrListT)
+import Control.Monad.ListT (ListT(..), ListItem(..))
 import Control.Monad.Trans (MonadTrans(..))
 import Data.Function (fix)
+import Data.Maybe (fromJust)
 import Prelude hiding (
   filter, repeat, scanl, takeWhile, zip, zipWith)
 
@@ -31,38 +32,32 @@ import Prelude hiding (
 -- Every list has an underlying monad.
 class (MonadPlus l, Monad (ItemM l)) => List l where
   type ItemM l :: * -> *
+  runList :: l a -> ItemM l (ListItem l a)
   -- | Transform an action returning a list to the returned list
   --
   -- > > joinL $ Identity "hello"
   -- > "hello"
-  joinL :: ItemM l (l b) -> l b
-  -- | foldr for 'List's.
-  -- the result and 'right side' values are monadic actions.
-  foldrL :: (a -> ItemM l b -> ItemM l b) -> ItemM l b -> l a -> ItemM l b
-  foldrL consFunc nilFunc = foldrL consFunc nilFunc . toListT
-  -- | Convert to a 'ListT'.
-  --
-  -- Can be done with a foldrL but included in type-class for efficiency.
-  toListT :: l a -> ListT (ItemM l) a
-  toListT = convList
-  -- | Convert from a 'ListT'.
-  --
-  -- Can be done with a foldrL but included in type-class for efficiency.
-  fromListT :: ListT (ItemM l) a -> l a
-  fromListT = convList
+  joinL :: ItemM l (l a) -> l a
 
 instance List [] where
   type ItemM [] = Identity
+  runList [] = Identity Nil
+  runList (x:xs) = Identity $ Cons x xs
   joinL = runIdentity
-  foldrL = foldr
-  toListT = fromList
 
 instance Monad m => List (ListT m) where
   type ItemM (ListT m) = m
-  joinL = ListT . (>>= runListT)
-  foldrL = foldrListT
-  toListT = id
-  fromListT = id
+  runList = runListT
+  joinL = ListT . (>>= runList)
+
+-- | foldr for 'List's.
+-- the result and 'right side' values are monadic actions.
+foldrL :: List l => (a -> ItemM l b -> ItemM l b) -> ItemM l b -> l a -> ItemM l b
+foldrL consFunc nilFunc list = do
+  item <- runList list
+  case item of
+    Nil -> nilFunc
+    Cons x xs -> consFunc x (foldrL consFunc nilFunc xs)
 
 -- | Prepend an item to a 'MonadPlus'
 cons :: MonadPlus m => a -> m a -> m a
@@ -96,39 +91,33 @@ filter cond =
       | cond x = return x
       | otherwise = mzero
 
--- for foldlL and scanl
-foldlL' :: List l =>
-  (a -> (ItemM l) c -> c) -> (a -> c) ->
-  (a -> b -> a) -> a -> l b -> c
-foldlL' joinVals atEnd step startVal =
-  t startVal . foldrL astep (return atEnd)
-  where
-    astep x rest = return $ (`t` rest) . (`step` x)
-    t cur = joinVals cur . (`ap` return cur)
-
 -- | An action to do foldl for 'List's
 foldlL :: List l => (a -> b -> a) -> a -> l b -> ItemM l a
-foldlL step startVal =
-  foldlL' (const join) id astep (return startVal)
-  where
-    astep rest x = liftM (`step` x) rest
+foldlL step startVal list = do
+  item <- runList list
+  case item of
+    Nil -> return startVal
+    Cons x xs ->
+      let v = step startVal x
+      in v `seq` foldlL step v xs
 
 scanl :: List l => (a -> b -> a) -> a -> l b -> l a
-scanl =
-  foldlL' consJoin $ const mzero
-  where
-    consJoin cur = cons cur . joinL
+scanl step startVal list =
+  cons startVal . joinL $ do
+    item <- runList list
+    return $ case item of
+      Nil -> mzero
+      Cons x xs -> scanl step (step startVal x) xs
 
 genericTake :: (Integral i, List l) => i -> l a -> l a
-genericTake count
-  | count <= 0 = const mzero
-  | otherwise = foldlL' joinStep (const mzero) next Nothing
-  where
-    next Nothing x = Just (count, x)
-    next (Just (i, _)) y = Just (i - 1, y)
-    joinStep Nothing = joinL
-    joinStep (Just (1, x)) = const $ return x
-    joinStep (Just (_, x)) = cons x . joinL
+genericTake count list
+  | count <= 0 = mzero
+  | otherwise =
+    joinL $ do
+      item <- runList list
+      return $ case item of
+        Nil -> mzero
+        Cons x xs -> cons x (genericTake (count-1) xs)
 
 -- | Execute the monadic actions in a 'List'
 execute :: List l => l a -> ItemM l ()
@@ -142,9 +131,8 @@ joinM :: List l => l (ItemM l a) -> l a
 joinM =
   joinL . foldrL consFunc (return mzero)
   where
-    consFunc action rest = do
-      x <- action
-      return . cons x . joinL $ rest
+    consFunc action rest =
+      liftM (`cons` joinL rest) action
 
 takeWhile :: List l => (a -> Bool) -> l a -> l a
 takeWhile cond =
@@ -152,7 +140,7 @@ takeWhile cond =
   where
     step x
       | cond x = return . cons x . joinL
-      | otherwise = const $ return mzero
+      | otherwise = const (return mzero)
 
 -- | An action to transform a 'List' to a list
 --
@@ -160,7 +148,7 @@ takeWhile cond =
 -- > "hello!"
 toList :: List l => l a -> ItemM l [a]
 toList =
-  foldrL step $ return []
+  foldrL step (return [])
   where
     step = liftM . (:)
 
@@ -194,22 +182,16 @@ liftListMonad ::
 liftListMonad = transformListMonad lift
 
 zip :: List l => l a -> l b -> l (a, b)
-zip as bs =
-  r0 (toListT as) (toListT bs)
-  where
-    r0 xx yy =
-      joinL $ do
-        xi <- runListT xx
-        case xi of
-          Nil -> return mzero
-          Cons x xs -> r1 x xs yy
-    r1 :: List l => a -> ListT (ItemM l) a -> ListT (ItemM l) b -> ItemM l (l (a, b))
-    r1 x xs yy = do
-      yi <- runListT yy
-      return $ case yi of
-        Nil -> mzero
-        Cons y ys ->
-          cons (x, y) $ r0 xs ys
+zip xx yy =
+  joinL $ do
+    xi <- runList xx
+    case xi of
+      Nil -> return mzero
+      Cons x xs -> do
+        yi <- runList yy
+        return $ case yi of
+          Nil -> mzero
+          Cons y ys -> cons (x, y) (zip xs ys)
 
 -- zipWith based on zip and not vice versa,
 -- because the other way around hlint compains "use zip".
@@ -221,17 +203,17 @@ zipWith func as = liftM (uncurry func) . zip as
 -- > > runIdentity $ lastL "hello"
 -- > 'o'
 lastL :: List l => l a -> ItemM l a
-lastL = foldlL (const id) undefined
+lastL = liftM fromJust . foldlL (const Just) Nothing
 
 repeat :: MonadPlus m => a -> m a
 repeat = fix . cons
 
 transpose :: List l => l (l a) -> l (l a)
 transpose matrix =
-  joinL $ toList matrix >>= r . map toListT
+  joinL $ toList matrix >>= r
   where
     r xs = do
-      items <- mapM runListT xs
+      items <- mapM runList xs
       return $ case filter isCons items of
         [] -> mzero
         citems ->
@@ -253,9 +235,9 @@ mergeOn f = joinL . foldlL (merge2On f) mzero
 -- > "01235689"
 merge2On :: (Ord b, List l) => (a -> b) -> l a -> l a -> l a
 merge2On f xx yy =
-  fromListT . joinL $ do
-    xi <- runListT (toListT xx)
-    yi <- runListT (toListT yy)
+  joinL $ do
+    xi <- runList xx
+    yi <- runList yy
     return $ case (xi, yi) of
       (Cons x xs, Cons y ys)
         | f y > f x -> cons x . merge2On f xs $ cons y ys
