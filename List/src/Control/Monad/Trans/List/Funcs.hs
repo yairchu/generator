84eedb6a8b78592491196734f1821599963b4812
Yair Chuchem <yairc@tiny.local>
2010-05-22 18:24:40 +0300

    Control.Monad.Trans.List.Funcs.repeat

--- List/src/Control/Monad/Trans/List/Funcs.hs
+++ List/src/Control/Monad/Trans/List/Funcs.hs
@@ -4,17 +4,21 @@
 -- Functions where the @List@ is an input type and not only the result type do not need special limited versions.
 
 module Control.Monad.Trans.List.Funcs
-    ( iterateM, repeatM, fromList
+    ( iterateM, repeatM, repeat, fromList
     ) where
 
 import Control.Monad.ListT (ListT)
 import qualified Data.List.Class as ListFuncs
+import Prelude hiding (repeat)
 
 iterateM :: Monad m => (a -> m a) -> m a -> ListT m a
 iterateM = ListFuncs.iterateM
 
 repeatM :: Monad m => m a -> ListT m a
 repeatM = ListFuncs.repeatM
+
+repeat :: Monad m => a -> ListT m a
+repeat = ListFuncs.repeat
 
 fromList :: Monad m => [a] -> ListT m a
 fromList = ListFuncs.fromList
