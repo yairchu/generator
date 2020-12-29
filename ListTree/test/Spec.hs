c7acf5731f9189a8b352930f35c74e8df9c57e29
Yair Chuchem <yairchu@gmail.com>
2020-12-29 17:14:35 +0200

    Add test (that catches infinite loop, oops)

---
+++ ListTree/test/Spec.hs
@@ -0,0 +1,23 @@
+import Control.Monad
+import Control.Monad.Generator
+import Control.Monad.Trans.Class
+import Data.List.Tree
+import Data.Maybe
+
+pythagorianTriplets :: [(Integer, Integer, Integer)]
+pythagorianTriplets =
+  catMaybes .
+  fmap fst .
+  bestFirstSearchSortedChildrenOn snd .
+  generate $ do
+    x <- lift [1..]
+    yield (Nothing, x)
+    y <- lift [1..]
+    yield (Nothing, x + y)
+    z <- lift [1..]
+    yield (Nothing, x + y + z)
+    lift . guard $ x*x + y*y == z*z
+    yield (Just (x, y, z), 0)
+
+main :: IO ()
+main = print $ take 10 pythagorianTriplets
